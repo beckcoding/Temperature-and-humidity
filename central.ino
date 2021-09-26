7c3aa3ff9d0d038f2b45eea920b63593ce4f0256
beckcoding <78371678+beckcoding@users.noreply.github.com>
2021-09-26 12:21:49 +0200

    Add files via upload

---
+++ central.ino
@@ -0,0 +1,41 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+
+
+#define BLYNK_PRINT Serial
+
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <BlynkSimpleEthernet.h>
+
+
+char auth[] = "acf82709ec4946fd81fafce04c0aa80d";
+
+#define W5100_CS  10
+#define SDCARD_CS 4
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  pinMode(SDCARD_CS, OUTPUT);
+  digitalWrite(SDCARD_CS, HIGH); // Deselect the SD card
+
+  Blynk.begin(auth);
+  // You can also specify server:
+  //Blynk.begin(auth, "blynk-cloud.com", 8442);
+  //Blynk.begin(auth, IPAddress(192,168,1,100), 8442);
+  // For more options, see Boards_Ethernet/Arduino_Ethernet_Manual example
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
